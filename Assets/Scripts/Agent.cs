71f7fc911c17e38fa77104a0471267c22d313dcf
choijongha <80575094+choijongha@users.noreply.github.com>
2022-04-06 09:08:21 +0900

    Agent에 물리적 조정을 위한 추가

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -8,13 +8,15 @@ public class Agent : MonoBehaviour
     public float orientation;
     public float rotation;
     public Vector3 velocity;
-
     protected Steering steering;
-
+    // 물리
+    private Rigidbody aRigidbody;
     private void Start()
     {
         velocity = Vector3.zero;
         steering = new Steering();
+
+        aRigidbody = GetComponent<Rigidbody>();
     }
     public void SetSteering(Steering steering)
     {
@@ -23,6 +25,8 @@ public class Agent : MonoBehaviour
     // 현재 값에 따라 이동 뼈대
     public virtual void Update()
     {
+        if (aRigidbody == null)
+            return;
         Vector3 displacement = velocity * Time.deltaTime;
         orientation += rotation * Time.deltaTime;
         // 회전 값들의 범위를 0에서 360사이로 제한해야 함
@@ -37,6 +41,8 @@ public class Agent : MonoBehaviour
     // 현재 프레임의 계산에 따라 다음 프레임의 움직임 갱신.
     public virtual void LateUpdate()
     {
+        if (aRigidbody == null)
+            return;
         velocity += steering.linear * Time.deltaTime;
         rotation += steering.angular * Time.deltaTime;
         if(velocity.magnitude > maxSpeed)
@@ -53,5 +59,26 @@ public class Agent : MonoBehaviour
             velocity = Vector3.zero;
         }
         steering = new Steering();
+
+        // 물리 엔진 위에서 작업하고 있기 때문에
+        // 오브젝트를 임의로 변환하는 대신 강체에 힘을 적용.
+        Vector3 displacement = velocity * Time.deltaTime;
+        orientation += rotation * Time.deltaTime;
+        if (orientation < 0.0f)
+            orientation += 360.0f;
+        else if (orientation > 360.0f)
+            orientation -= 360.0f;
+        // 무엇을 하고 싶은지에 따라 ForceMode 값 설정
+        // 여기서는 보여주는 용도로 VelocityChange를 사용.
+        aRigidbody.AddForce(displacement, ForceMode.VelocityChange);
+        Vector3 orientationVector = OriToVec(orientation);
+        aRigidbody.rotation = Quaternion.LookRotation(orientationVector, Vector3.up);
+    }
+    public Vector3 OriToVec(float orientation)
+    {
+        Vector3 vector = Vector3.zero;
+        vector.x = Mathf.Sin(orientation * Mathf.Deg2Rad) * 1.0f;
+        vector.y = Mathf.Cos(orientation * Mathf.Deg2Rad) * 1.0f;
+        return vector.normalized;
     }
 }
